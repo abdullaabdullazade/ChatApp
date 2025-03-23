9df63721e10ae6fbe5c82b4f1aa04d8e1304f4d3
Abdulla Abdullazade <abdulla.abdullazade.2008@gmail.com>
2025-03-23 19:07:21 +0400

    main

--- app/(stack)/firebaseConfig.js
+++ app/(stack)/firebaseConfig.js
@@ -4,14 +4,7 @@ import { getDatabase } from 'firebase/database';
 import { getMessaging, getToken } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZgALFpxEQ2Nf-2BNlkokQVgO-3ktJVNc",
-  authDomain: "chatapp-40574.firebaseapp.com",
-  databaseURL: "https://chatapp-40574-default-rtdb.firebaseio.com",
-  projectId: "chatapp-40574",
-  storageBucket: "chatapp-40574.firebasestorage.app",
-  messagingSenderId: "745736489767",
-  appId: "1:745736489767:web:bd9ab0218452d743dc3702",
-  measurementId: "G-7HF9REJRN1"
+
 };
 
 const firebase = initializeApp(firebaseConfig);
